0087aed7444a953dd59e4ea8003d8bce6341f5b8
Francis Anthony R. Aludo <francisanthonyaludo@gmail.com>
2026-05-31 14:11:12 +0800

    feat: replace static array with Fetch API calls to Spring Boot backend

--- src/main/java/com/ws101/aludoofilanda/EcommerceApi/Controller/ProductController.java
+++ src/main/java/com/ws101/aludoofilanda/EcommerceApi/Controller/ProductController.java
@@ -1,3 +1,10 @@
+
+@RestController
+@RequestMapping("/api/v1/products")
+@CrossOrigin(origins = "*")
+public class ProductController {
+
+
 package com.ws101.aludoofilanda.EcommerceApi.Controller;
 
 import com.ws101.aludoofilanda.EcommerceApi.Model.ProductModel;
